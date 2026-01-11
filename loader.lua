9da9c7c3cb923e3efdacdfb70f92e16d5cff1d35
Ahmad <marwanabram1234@gmail.com>
2026-01-11 11:22:19 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,7 +4,8 @@ local HttpService = game:GetService("HttpService")
 local placeId = game.PlaceId
 
 local scripts = {
-    [3101667897] = "https://api.luarmor.net/files/v4/loaders/8ef3f808503edde6a153f8d96b3399de.lua" -- Legends Of Speed
+    [3101667897] = "https://api.luarmor.net/files/v4/loaders/8ef3f808503edde6a153f8d96b3399de.lua", -- Legends Of Speed
+    [3623096087] = "https://api.luarmor.net/files/v4/loaders/0af0f34c0584457a069b941a757083a5.lua" -- Muscle Legends
 }
 
 local url = scripts[placeId]
